5c512bbb1a8aa20bc00b447864daa5042725add3
caitl <caitl@SurfaceKitty6>
2020-05-07 16:41:37 -0700

    DynamicIntegerArray, started IntroToArrayLists   5/7/20

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -4,20 +4,28 @@ import java.util.ArrayList;
 
 public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> list = new ArrayList<String>();
+		// 2. Add five Strings to your list
+		list.add("hi");
+		list.add("salutations");
+		list.add("heeey");
+		list.add("wassup");
+		list.add("hello there");
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < list.size(); i++) {
+			String s = list.get(i);
+			System.out.println("String at element " + i + " " + s);
+		}
+		System.out.println("\n");
 
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 4. Print all the Strings using a for-each loop
+
+		// 5. Print only the even numbered elements in the list.
+
+		// 6. Print all the Strings in reverse order.
+
+		// 7. Print only the Strings that have the letter 'e' in them.
 	}
 }
