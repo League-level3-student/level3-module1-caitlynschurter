79ec256052662da2dc8e4176c099c47d9381f63a
caitl <caitl@SurfaceKitty6>
2020-06-18 16:30:01 -0700

    Finished IntegerStack, started IntroToStack   6/18/20

--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -1,50 +1,57 @@
 package _02_Integer_Stack;
 
+import java.util.ArrayList;
+
 public class IntegerStack {
 	//1. create a private array of integers
-	
+	private int list[];
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
 	public IntegerStack() {
+		list = new int[0];
 	}
 	
 	//3. Complete the steps in the push method.
 	public void push(int v) {
 		//A. create a new array that is one element larger than the member array
-		
+		int listB[] = new int[list.length+1];
 		//B. set the last element of the new array equal to the integer passed into the method
-		
+		listB[listB.length-1]= v;
 		//C. iterate through the member array and copy all the elements into the new array
-		
+		for(int i = 0; i < list.length; i++) {
+			listB[i] = list[i];
+		}
 		//D. set the member array equal to the new array.
-		
+		list = listB;
 	}
 	
 	//4. Complete the steps in the pop method.
 	public int pop() {
 		//A. create an integer variable and initialize it to the
 		//   last element of the member array.
-		
+		int temp = list[list.length-1];
 		//B. create a new array that is one element smaller than the member array
-		
+		int listC[] = new int[list.length-1];
 		//C. iterate through the new array and copy every element from the
 		//   member array to the new array
-		
+		for(int i = 0; i < listC.length; i++) {
+			listC[i] = list[i];
+		}
 		//D. set the member array equal to the new array
-		
+		list = listC;
 		//E. return the variable you created in step A
-		return 0;
+		return temp;
 	}
 	
 	//5. Complete the clear method to set the
 	//   member array to a new array of length 0
 	public void clear() {
-		
+		list = new int[0];
 	}
 	
 	//6. Complete the size method to return 
 	//   the length of the member array
 	public int size() {
-		return 0;
+		return list.length;
 	}
 }
--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,11 +9,14 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> stack = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		Random rand = new Random();
+		for(int i =0; i<100; i++) {
+			stack.push(rand.nextDouble()*100);	
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		JOptionPane.showInputDialog("Enter two numbers between 0 and 100");
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
 		
