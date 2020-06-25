f8ea0faff756c6193fc0f4413888d3467b218ce1
caitl <caitl@SurfaceKitty6>
2020-06-25 16:28:28 -0700

    Finished IntroToStack, started TextUndoRedo   6/25/20

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -7,32 +7,55 @@ import javax.swing.JOptionPane;
 
 public class _01_IntroToStack {
 	public static void main(String[] args) {
-		//1. Create a Stack of Doubles
-		//   Don't forget to import the Stack class
+		// 1. Create a Stack of Doubles
+		// Don't forget to import the Stack class
 		Stack<Double> stack = new Stack<Double>();
-		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+		// 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
 		Random rand = new Random();
-		for(int i =0; i<100; i++) {
-			stack.push(rand.nextDouble()*100);	
+		for (int i = 0; i < 100; i++) {
+			stack.push(rand.nextDouble() * 100);
 		}
-		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		JOptionPane.showInputDialog("Enter two numbers between 0 and 100");
-		//4. Pop all the elements off of the Stack. Every time a double is popped that is
-		//   between the two numbers entered by the user, print it to the screen.
+		// 3. Ask the user to enter in two numbers between 0 and 100, inclusive.
+		int firstInt = Integer.parseInt(JOptionPane.showInputDialog("Enter a number between 0 and 100"));
+		int secondInt = Integer.parseInt(JOptionPane.showInputDialog("Enter another number between 0 and 100"));
+		// 4. Pop all the elements off of the Stack. Every time a double is popped that
+		// is
+		// between the two numbers entered by the user, print it to the screen.
+		int greater = 0;
+		int smaller = 0;
 		
-		
-		//   EXAMPLE:
-		//   NUM 1: 65
-		//   NUM 2: 75
-		
-		//   Popping elements off stack...
-		//   Elements between 65 and 75:
-		//   66.66876846
-		//   74.51651681
-		//   70.05110654
-		//   69.21350456
-		//   71.54506465
-		//   66.47984807
-		//   74.12121224
+		if (firstInt > secondInt) {
+			greater = firstInt;
+			smaller = secondInt;
+		}
+
+		else if (secondInt > firstInt) {
+			greater = secondInt;
+			smaller = firstInt;
+		}
+
+		else {
+			greater = firstInt;
+			smaller = secondInt;
+		}
+		for (int i = 0; i < stack.size(); i++) {
+			double d = stack.pop();
+			if(d<greater && d>smaller) {
+				System.out.println(d);
+			}
+		}
+		// EXAMPLE:
+		// NUM 1: 65
+		// NUM 2: 75
+
+		// Popping elements off stack...
+		// Elements between 65 and 75:
+		// 66.66876846
+		// 74.51651681
+		// 70.05110654
+		// 69.21350456
+		// 71.54506465
+		// 66.47984807
+		// 74.12121224
 	}
 }
